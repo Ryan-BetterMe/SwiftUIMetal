767fab08ea8ab2e0de3f9e8bc76ef4a6ae22fd5d
Ryan <lancexianghui@gmail.com>
2023-12-29 23:51:01 +0800

    Create Project

---
+++ SwiftUIMetal/SwiftUIMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMetal
+//
+//  Created by 向辉 on 2023/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMetal/SwiftUIMetal/SwiftUIMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMetalApp.swift
+//  SwiftUIMetal
+//
+//  Created by 向辉 on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
